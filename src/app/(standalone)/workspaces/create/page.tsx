92f4cc974b3154d925b3bd608911d101a0b41664
Vinayak Vohra <vn26vhr@gmail.com>
2024-12-04 12:12:07 +0530

    fix: lint error

--- src/app/(standalone)/workspaces/create/page.tsx
+++ src/app/(standalone)/workspaces/create/page.tsx
@@ -13,7 +13,7 @@ export default async function CreateWorkspacePage() {
 
   return (
     <div className="min-h-[calc(90vh-90px)] py-5 flex flex-col items-center justify-evenly gap-5 w-full md:gap-0 md:max-w-5xl">
-      <p className="text-2xl font-semibold">Let's get you started!</p>
+      <p className="text-2xl font-semibold">Let&apos;s get you started!</p>
       <div className="flex w-full gap-4 max-md:flex-col">
         <CreateWorkpaceForm defaultName={getWorkspaceName(user.name)} />
         {/* <div className="bg-background rounded-full h-1 w-auto lg:h-auto lg:w-1" />
