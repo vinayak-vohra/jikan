526a16f2eed4926b31462b72ea199ec4671ea897
Vinayak Vohra <vn26vhr@gmail.com>
2024-12-05 20:19:28 +0530

    feat: add project retrieval and redirect handling in ProjectId and ProjectSettings pages

--- src/app/(standalone)/workspaces/[workspaceId]/projects/[projectId]/settings/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/projects/[projectId]/settings/page.tsx
@@ -1,11 +1,23 @@
 import { getCurrentUser } from "@/features/auth/services";
 import { redirect } from "next/navigation";
 import ProjectSettingsClient from "./client";
+import { getProjectById } from "@/features/projects/services/get-project-by-id";
 
+interface ProjectSettingsProps {
+  params: {
+    workspaceId: string;
+    projectId: string;
+  };
+}
 
-export default async function ProjectSettingsPage() {
+export default async function ProjectSettingsPage({
+  params,
+}: ProjectSettingsProps) {
   const user = await getCurrentUser();
   if (!user) redirect("/sign-in");
+
+  const project = await getProjectById(params.projectId, user);
+  if (!project) redirect(`/workspaces/${params.workspaceId}`);
 
   return <ProjectSettingsClient />;
 }
