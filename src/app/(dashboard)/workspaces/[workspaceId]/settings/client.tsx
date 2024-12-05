d620afabe89e0022e87425ff0b111b940a4e0648
Vinayak Vohra <vn26vhr@gmail.com>
2024-12-05 20:20:27 +0530

    feat: implement error handling with ErrorCard component in various workspace pages

--- src/app/(dashboard)/workspaces/[workspaceId]/settings/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/settings/client.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { TitleCard } from "@/components/cards/title-card";
-import ErrorPage from "@/components/error";
+import ErrorCard from "@/components/error";
 import Loader from "@/components/loader";
+import { Button } from "@/components/ui/button";
 import {
   WorkspaceDeleteCard,
   WorkspaceEditCard,
@@ -9,14 +10,27 @@ import {
 } from "@/features/workspaces/components/cards";
 import { useWorkspaceId } from "@/features/workspaces/hooks";
 import { useFetchWorkspaceById } from "@/features/workspaces/hooks";
+import { RotateCwIcon } from "lucide-react";
 
 export default function WorkspaceSettingsClient() {
   const workspaceId = useWorkspaceId();
-  const { data: currentWorkspace, isLoading } =
-    useFetchWorkspaceById(workspaceId);
+  const {
+    data: currentWorkspace,
+    isLoading,
+    error,
+    refetch,
+  } = useFetchWorkspaceById(workspaceId);
 
   if (isLoading) return <Loader />;
-  if (!currentWorkspace) return <ErrorPage message="Workspace not found" />;
+  if (error)
+    return (
+      <ErrorCard
+        title={error.title}
+        message={error.message}
+        refetch={refetch}
+      />
+    );
+  if (!currentWorkspace) return null;
 
   return (
     <div className="w-full sm:max-w-md md:max-w-lg mx-auto flex flex-col gap-y-2 md:gap-y-4">
--- src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import ErrorPage from "@/components/error";
+import ErrorCard from "@/components/error";
 import Loader from "@/components/loader";
 import TaskBreadCrumbs from "@/features/tasks/components/task-breadcrumbs";
 import TaskDescription from "@/features/tasks/components/task-description";
@@ -9,11 +9,19 @@ import { useFetchTaskById, useTaskId } from "@/features/tasks/hooks";
 
 export default function TaskIdClient() {
   const taskId = useTaskId();
-  const { data, isLoading } = useFetchTaskById(taskId);
+  const { data, isLoading, error, refetch } = useFetchTaskById(taskId);
 
   if (isLoading) return <Loader />;
+  if (error)
+    return (
+      <ErrorCard
+        title={error.title}
+        message={error.message}
+        refetch={refetch}
+      />
+    );
+  if (!data) return null;
 
-  if (!data) return <ErrorPage message="Task Not Found" />;
   return (
     <div className="flex w-full flex-col gap-y-4">
       <TaskBreadCrumbs project={data.project} task={data} />
