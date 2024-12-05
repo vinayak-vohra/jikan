5240a096db5f21559e8bca095a492d17f0c17ff2
Vinayak Vohra <vn26vhr@gmail.com>
2024-12-05 20:18:41 +0530

    feat: implement invite code verification and error handling in JoinPage

--- src/app/(standalone)/workspaces/[workspaceId]/invite/[inviteCode]/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/invite/[inviteCode]/page.tsx
@@ -3,8 +3,17 @@ import { redirect } from "next/navigation";
 import { getCurrentUser } from "@/features/auth/services";
 
 import InviteClient from "./client";
+import { getWorkspaceById } from "@/features/workspaces/services";
+import ErrorCard from "@/components/error";
 
-export default async function JoinPage() {
+interface JoinPageProps {
+  params: {
+    workspaceId: string;
+    inviteCode: string;
+  };
+}
+
+export default async function JoinPage({ params }: JoinPageProps) {
   const user = await getCurrentUser();
   if (!user) redirect("/sign-in");
   // redirect(
@@ -12,6 +21,17 @@ export default async function JoinPage() {
   //     `/workspaces/${params.workspaceId}/invite/${params.inviteCode}`
   //   )}`
   // );
+
+  const workspace = await getWorkspaceById(params.workspaceId);
+  if (!workspace) redirect("/");
+
+  if (workspace.inviteCode !== params.inviteCode)
+    return (
+      <ErrorCard
+        title="Invalid invite code"
+        message="The invite code you provided is invalid. Please check the link and try again."
+      />
+    );
 
   return (
     <div className="py-5 flex flex-col items-center justify-evenly gap-5 w-full md:gap-0 md:max-w-5xl">
